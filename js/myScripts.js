6b86cf6b792ee057e56b3193725ca7541871b2ba
tedda42 <tedda42@gmail.com>
2015-10-30 19:20:19 +1100

    first commit

---
+++ js/myScripts.js
@@ -0,0 +1,11 @@
+/**
+ * Created by i on 30/10/15.
+ */
+// Can also be used with $(document).ready()
+
+//$(window).load(function() {
+$(document).ready(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+});
